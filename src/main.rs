bf722e1017527585005a78b5411bd46bf353bf75
Andres Crucetta Nieto <acrucettanieto@gmail.com>
2023-01-28 15:04:52 -0600

    pushing all files

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use anki_builder_rust::Config;
+use anki_builder_rust::run;
+use std::env;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+        std::process::exit(1);
+    }
+}
+
